d71e78dd390015ab6f36c8e42efe11c003388dd9
Mihail87 <leontemihai@gmail.com>
2019-09-13 08:23:43 +0300

    Project setup

---
+++ SwiftUI_BullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_BullsEye
+//
+//  Created by Mihai Leonte on 9/13/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
