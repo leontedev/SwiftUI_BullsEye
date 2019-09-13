1784488e33e44c3d19e0b186be4103d8a8f76d9f
Mihail87 <leontemihai@gmail.com>
2019-09-13 11:00:50 +0300

    Completed project

--- SwiftUI_BullsEye/ContentView.swift
+++ SwiftUI_BullsEye/ContentView.swift
@@ -9,10 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let targetValue = Int.random(in: 1..<100)
+    @State var guess: Float = 0.5
+    @State var showAlert: Bool = false
+    
+    func computeScore() -> String {
+        let difference: Int = abs(targetValue - Int(guess))
+        if difference == 0 {
+            return "Bull's Eye!"
+        } else {
+            return String(100 - difference)
+        }
+    }
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Put the Bull's Eye as close as you can to: \(targetValue)")
+            HStack {
+                Text("0")
+                Slider(value: $guess, in: 0.0...100.0, step: 1.0)
+                Text("100")
+            }
+            Button(action: { self.showAlert = true }) {
+                Text("Hit me!")
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Score"), message: Text(computeScore()))
+            }
+        }.padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
